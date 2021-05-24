2dff88f58a7c0e2cfe43d3eb2f546342d5a60cc4
Felipe Nascimento <felipe_nsousa@outlook.com>
2021-05-24 09:04:28 -0400

    Create models and user commands

---
+++ user.core/src/main/java/com/springbank/user/core/models/User.java
@@ -0,0 +1,25 @@
+package com.springbank.user.core.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Document(collection = "users")
+public class User {
+
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Account account;
+
+}
+
